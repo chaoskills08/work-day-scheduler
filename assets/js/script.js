71658244719220d56b8581562f09b9d4543bdc69
Nicholas Bales <nickb96@icloud.com>
2022-08-11 19:47:09 -0400

    added functioning timeblock color code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,37 @@
+var hours = document.getElementById('.time-block');
+
 var currentDay = moment();
 $("#currentDay").text(currentDay.format('dddd, MMMM Do'));
+
+function timeColor() {
+  var hour = moment().hours();
+  console.log(hour);
+
+  $(".time-block").each(function () {
+    var currHour = parseInt($(this).attr("id"));
+    if (currHour < hour) {
+      $(this).addClass("past");
+    } else if (currHour === hour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    } console.log(currHour);
+  });
+}
+timeColor();
+
+// function timeof(); {
+//   for (var i = 0; i < hours.length; i++) {
+//     var thisHour = hours[i].dataset.hour;
+//     if (thisHour < hourNow) {
+//       hours[i].classList.add(".past");
+//     } else if (thisHour === hourNow) {
+//       hours[i].classList.add(".present");
+//     } else (thisHour > hourNow); {
+//       hours[i].classList.add(".future");
+//     }
+//   }
+// }
+// timeOf();
+
 
